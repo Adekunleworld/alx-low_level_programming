2efe06b6245e632d6a2eff6cf30de11a3870e071
divijoy001@gmail.com <divijoy001@gmail.com>
2023-06-15 19:59:51 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,7 +8,8 @@
  */
 int main(void)
 {
-    printf("\"Programming is like building a multilingual puzzle, but the outcome is a piece of art,\n");
-    return 0;
+	printf("Programming is like building a multilingual puzzle, ");
+	printf("but the outcome is a piece of art,\n");
+	return (0);
 }
 
