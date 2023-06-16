17b82444c314d1358b387211a4e7f0fe6966c9f1
divijoy001@gmail.com <divijoy001@gmail.com>
2023-06-15 19:02:47 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,7 @@
 /**
  * main - Entry point of the program
  *
+ * Description: Prints a message using the puts function
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -10,3 +11,4 @@ int main(void)
 	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
+
