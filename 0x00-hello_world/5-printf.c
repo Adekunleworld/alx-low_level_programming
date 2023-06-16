4d275ca8cd3596cad39c905ba2d4a0e8b3bd0825
divijoy001@gmail.com <divijoy001@gmail.com>
2023-06-15 19:36:24 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a message with proper grammar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    printf("\"Programming is like building a multilingual puzzle, but the outcome is a piece of art,\n");
+    return 0;
+}
+
