04f9044c00570bbf44f19f9e2b32f7b649b0e9f3
divijoy001@gmail.com <divijoy001@gmail.com>
2023-06-18 15:14:18 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,18 @@
 #include <unistd.h>
 
 /**
- * main - Entry point of the program
+ * main - Entry point
  *
- * Return: Always 1 (indicating an error)
+ * Description: Prints a specific message to the standard error
+ * Return: 1
  */
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, message, 59);
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	ssize_t len = 59;
+
+	if (write(2, msg, len) != len)
+		return (1);
 	return (1);
 }
+
