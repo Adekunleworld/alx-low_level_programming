4dacf1fd54f912cb8a56f1a60d799e379cc1ec44
divijoy001@gmail.com <divijoy001@gmail.com>
2023-06-30 21:21:29 -0700

    100-rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+
+/**
+ * rot13 - Encodes a string using ROT13.
+ * @str: The string to be encoded.
+ *
+ * Return: The encoded string.
+ */
+char *rot13(char *str)
+{
+	char *ptr = str;
+	char *alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; alpha[j] != '\0'; j++)
+		{
+			if (str[i] == alpha[j])
+			{
+				str[i] = rot13[j];
+				break;
+			}
+		}
+	}
+
+	return (ptr);
+}
