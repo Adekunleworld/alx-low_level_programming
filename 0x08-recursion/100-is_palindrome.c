2c5aedc0afbe3a367f16f9d43a2f5e05a222818f
divijoy001@gmail.com <divijoy001@gmail.com>
2023-07-06 20:38:58 -0700

    100-palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,7 +8,8 @@
  * Return: 1 if palindrome, 0 otherwise.
  */
 int is_palindrome(char *s);
-int _is_palindrome(char *s, int len);
+int _is_palindrome(char *s, int start, int end);
+int _strlen_recursion(char *s);
 
 /**
  * is_palindrome - Checks if a string is a palindrome.
@@ -18,31 +19,42 @@ int _is_palindrome(char *s, int len);
  */
 int is_palindrome(char *s)
 {
-	int len = 0;
-
 	if (*s == '\0')
 		return (1);
 
-	while (*(s + len) != '\0')
-		len++;
-
-	return (_is_palindrome(s, len - 1));
+	return (_is_palindrome(s, 0, _strlen_recursion(s) - 1));
 }
 
 /**
- * _is_palindrome - Helper function to check if a string is a palindrome.
+ * is_palindrome - Helper function to check if a string is a palindrome.
  * @s: The string to be checked.
- * @len: The length of the string.
+ * @start: The starting index of the substring.
+ * @end: The ending index of the substring
  *
- * Return: 1 if palindrome, 0 otherwise.
+ * Return: 1 if the string is a palindrome, 0 otherwise.
  */
-int _is_palindrome(char *s, int len)
+int _is_palindrome(char *s, int start, int end)
 {
-	if (len <= 0)
+	if (start >= end)
 		return (1);
 
-	if (*s != *(s + len))
+	if (s[start] != s[end])
 		return (0);
 
-	return (_is_palindrome(s + 1, len - 2));
+	return (_is_palindrome(s, start + 1, end - 1));
+}
+
+/**
+
+ * _strlen_recursion - Helper function to calculate the length of a string
+ * @s: The string.
+ *
+ * Return: The length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+
+	return (1 + _strlen_recursion(s + 1));
 }
