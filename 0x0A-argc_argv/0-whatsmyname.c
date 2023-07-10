b0f8c57fd1034b98f7237da61a98d4348b837fe3
divijoy001@gmail.com <divijoy001@gmail.com>
2023-07-10 14:03:59 -0700

    mynewnameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Entry point of the program.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of strings containing the arguments.
+ *
+ * Return: 0 on success.
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n",argv[0]);
+
+	return (0);
+}
